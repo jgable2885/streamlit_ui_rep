6bdb434d646aa4ee4ca90fc4c1017e487eafb4af
luyingamypei <100870511+luyingamypei@users.noreply.github.com>
2023-04-02 16:29:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -52,20 +52,21 @@ if st.session_state['button'] == True:
 
 	preds = model_reload.predict_on_batch(new_smile3, transformers=transformers3)
 	preds_df = pd.DataFrame(preds[0], columns=['Prob False','Prob Tox'])
-	preds_df['Assay'] = tox21_tasks3
+	preds_df['Assay Name'] = tox21_tasks3
 	preds_df['Probability of Toxicity'] = preds_df['Prob Tox'].astype(float).map(lambda n: '{:.2%}'.format(n))
-
 	preds_df['tox_class'] = preds_df['Prob Tox'].apply(get_tox_class)
-	st.table(preds_df.loc[:,['Assay','tox_class', 'Probability of Toxicity']])
+	preds_df = preds_df.rename(columns= {'tox_class':'Toxicity Class'}, inplace = True)
+	
+	st.table(preds_df.loc[:,['Assay Name','Toxicity Class', 'Probability of Toxicity']])
 	input_tox_count = np.sum(preds_df['Prob Tox'] > 0.6)
 	st.write("{} predictions suggest toxicity".format(input_tox_count))
 
-	chart_data = pd.DataFrame(preds_df[['Assay','tox_class','Prob Tox']], columns=['Assay','tox_class', 'Prob Tox'])
+	chart_data = pd.DataFrame(preds_df[['Assay Name','Toxicity Class','Prob Tox','Probability of Toxicity']], columns=['Assay Name','Toxicity Class', 'Prob Tox','Probability of Toxicity'])
 
 	chart = alt.Chart(chart_data, title="Tox Predictions for Your Input Molecule").mark_bar(color='darkred').encode(
 		x = alt.X('Prob Tox:Q', scale=alt.Scale(domain=[0, 1.0]), axis=alt.Axis(format='%')),
-		y='Assay:N', 
-		tooltip=['Assay','tox_class', 'Prob Tox'])
+		y='Assay Name:N', 
+		tooltip=['Assay Name','Toxicity Class', 'Probability of Toxicity'])
 	st.altair_chart(chart, use_container_width=True) 
 	
 	ideas_df = generate_ideas(input_smile, database="AllHepG2.mmpdb")
