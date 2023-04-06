f566b398e9fdc351d883e467e296f3971a7460b2
jgable2885 <jgable@gmail.com>
2023-04-06 11:16:40 -0700

    Try sorting by similarity then toxicity

--- app.py
+++ app.py
@@ -122,12 +122,12 @@ if st.session_state['button'] == True:
 	sims = DataStructs.BulkTanimotoSimilarity(input_fp, fps)
 	tox21_y_data['sim'] = sims
 	tox21_y_data['toxicity_counts'] = tox21_y_data.loc[:,tox21_tasks3].sum(axis=1)
-	tox21_y_data.sort_values(by=['toxicity_counts','sim'], ascending=[True, False], inplace=True)
+	tox21_y_data.sort_values(by=['sim','toxicity_count'], ascending=[False, True], inplace=True)
 	sim_mols = [Chem.MolFromSmiles(x) for x in tox21_y_data['SMILES'][0:3]]
 	sim_legends = ["Experimental toxicities: {}".format(c) for c in tox21_y_data['toxicity_counts'][0:3]]
 	sim_grid = Chem.Draw.MolsToGridImage(sim_mols, legends=sim_legends)
 	sim_grid.save("sim_grid.png")
-	st.image(Image.open("sim_grid.png"), caption='Top 3 most similar compounds from Tox21 database')
+	st.image(Image.open("sim_grid.png"), caption='Top 3 most similar less toxic compounds from Tox21 database')
 	
 # 	grid_img = Chem.Draw.MolsToGridImage(idea_mols, legends=idea_legends[0:num_ideas], 
 # 										 highlightAtomLists=idea_hit_atoms, highlightBondLists=idea_hit_bonds)
