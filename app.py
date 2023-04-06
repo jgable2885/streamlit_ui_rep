cc9ccc946e0d6fe1314d6f49c14d055a4db3f23b
jgable2885 <jgable@gmail.com>
2023-04-06 11:26:43 -0700

    Tweak formating of legend for sim results

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ if st.session_state['button'] == True:
 	tox21_y_data['toxicity_counts'] = tox21_y_data.loc[:,tox21_tasks3].sum(axis=1)
 	tox21_y_data.sort_values(by=['sim','toxicity_counts'], ascending=[False, True], inplace=True)
 	sim_mols = [Chem.MolFromSmiles(x) for x in tox21_y_data['SMILES'][0:3]]
-	sim_legends = ["Experimental toxicities: {}  Similarity to input: {}".format(c, s) 
+	sim_legends = ["Experimental toxicities: {}\nSimilarity to input: {:.2%}".format(c, s) 
 					for c, s in zip(tox21_y_data['toxicity_counts'][0:3], tox21_y_data['sim'][0:3])]
 	sim_grid = Chem.Draw.MolsToGridImage(sim_mols, legends=sim_legends)
 	sim_grid.save("sim_grid.png")
